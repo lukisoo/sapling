d5da31d78abe38a5a1b0c62384905d34636436ac
Lucy Zhu <lukisoo@wifi-172-23-173-228.uoa-wifi.auckland.ac.nz>
2023-07-09 00:41:29 +1200

    garden start

--- client/src/pages/GardenPage.jsx
+++ client/src/pages/GardenPage.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import "./GardenPage.css";
+import RoadmapButton from "../assets/return-to-roadmap-button.png"
+// import Xx from "../assets/dirt.svg"
 
 export default function GardenPage() {
-    return <h1>Garden Page</h1>;
+    return (
+        <>
+            <div className="wrapper">
+                {/* <RoadmapButton/> */}
+                <img src={RoadmapButton}/>
+                <div>
+                    <h1>fhsodifh</h1>
+                <div className="store-bg"></div></div>
+                <div>Two</div>
+            </div>
+        </>);
 };
