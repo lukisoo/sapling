dd3347727e6cb267bc64300e8f65b3cac97179df
ylei532 <80793740+ylei532@users.noreply.github.com>
2023-07-08 20:41:24 +1200

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,19 @@ import HomePage from "./pages/HomePage";
 import GardenPage from "./pages/GardenPage";
 import LeaderboardPage from './pages/LeaderboardPage';
 import ProfilePage from './pages/ProfilePage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/garden" element={<GardenPage />}/>
-        <Route path="/profile" element={<ProfilePage />}/>
-        <Route path="/leaderboard" element={<LeaderboardPage />}/>
-    </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/garden" element={<GardenPage />}/>
+          <Route path="/profile" element={<ProfilePage />}/>
+          <Route path="/leaderboard" element={<LeaderboardPage />}/>
+      </Routes>
+    </div>
   )
 }
 
