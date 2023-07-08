b6e8428d48461014e96ac45da37a8980ae7761f9
Lucy Zhu <lukisoo@wifi-172-23-173-228.uoa-wifi.auckland.ac.nz>
2023-07-09 03:29:28 +1200

    save

---
+++ client/atoms/currentIslandState.jsx
@@ -0,0 +1,9 @@
+import { atom } from "recoil";
+
+// Takes index of current album in focus
+export const currentIslandState = atom({
+  key: "currentIslandState", // unique ID (with respect to other atoms/selectors)
+  // default: 0, // default value (aka initial value)
+
+  default: ["","","","","","","","","","","","","","","","","","","","","","",""], // default value (aka initial value)
+});
--- client/src/pages/GardenPage.jsx
+++ client/src/pages/GardenPage.jsx
@@ -2,11 +2,28 @@ import React from "react";
 import "./GardenPage.css";
 import RoadmapButton from "../assets/return-to-roadmap-button.png";
 import BirdTree from "../assets/bird-tree.png";
+import {
+    RecoilRoot,
+    atom,
+    selector,
+    useRecoilState,
+    useRecoilValue,
+  } from "recoil";
+  import { useState } from "react";
+
+
+
+import { currentIslandState } from "../../atoms/currentIslandState";
 
 import { NavLink } from "react-router-dom";
 // import Xx from "../assets/dirt.svg"
 
 export default function GardenPage() {
+    const [currentIslandDisplay, setCurrentIslandDisplay] =
+    useRecoilState(currentIslandState);
+
+    const [currentPlant, setCurrentPlant] = useState("")
+
   return (
     <>
       <div className="wrapper">
@@ -14,7 +31,10 @@ export default function GardenPage() {
           <NavLink exact to="/" activeClassName="active">
             <img className="roadmap-button" src={RoadmapButton} />
           </NavLink>
-          <h1>fhsodifh</h1>
+          <img onClick={() => {setCurrentIslandDisplay([BirdTree].concat(currentIslandState[1,23]))}} className="roadmap-button" src={RoadmapButton} />
+
+
+          <h1>{currentIslandDisplay[0]}</h1>
           <div className="store-bg"></div>
         </div>
 
@@ -23,13 +43,13 @@ export default function GardenPage() {
             {/* <img className="island" src={IslandBg} /> */}
             <div className="island-overlay">
               <div class="grid-container">
-                <div id="1" className="grid-box placeholder"></div>
+                <div id="1" className="grid-box placeholder">{currentIslandDisplay[0]}</div>
                 <div id="1" className="grid-box placeholder"></div>
                 <div id="1" className="grid-box placeholder"></div>
                 <div id="1" className="grid-box placeholder"></div>
 
                 <div id="1" className="grid-box valid">
-                  <img className="grid-icon" src={BirdTree} />
+                  <img className="grid-icon" src={currentIslandDisplay[0]} />
                 </div>
                 <div id="1" className="grid-box valid">
                   <img className="grid-icon" src={""} />
