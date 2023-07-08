8afd2ae78c63a7ddbbae048e8d18546c63ce3221
Lucy Zhu <lukisoo@wifi-172-23-173-228.uoa-wifi.auckland.ac.nz>
2023-07-09 05:03:52 +1200

    i hate coding i want to kms

--- client/src/pages/GardenPage.jsx
+++ client/src/pages/GardenPage.jsx
@@ -8,14 +8,13 @@ import FloweringTree from "../assets/plants/flowering-tree.png";
 import BirdTreeListing from "../assets/shop-listings/bird-tree.png";
 import FloweringTreeListing from "../assets/shop-listings/flowering-tree.png";
 
-
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from "recoil";
+// import {
+//   RecoilRoot,
+//   atom,
+//   selector,
+//   useRecoilState,
+//   useRecoilValue,
+// } from "recoil";
 import { useState } from "react";
 
 import { currentIslandState } from "../../atoms/currentIslandState";
@@ -24,10 +23,76 @@ import { NavLink } from "react-router-dom";
 // import Xx from "../assets/dirt.svg"
 
 export default function GardenPage() {
-  const [currentIslandDisplay, setCurrentIslandDisplay] =
-    useRecoilState(currentIslandState);
+  //   const [currentIslandDisplay, setCurrentIslandDisplay] =
+  //     useRecoilState(currentIslandState);
+
+  const [currentIslandDisplay, setCurrentIslandDisplay] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
 
   const [currentPlant, setCurrentPlant] = useState("");
+
+  const handlePurchase = (selectedPlant) => {
+    //TODO CHECK MONEY IF POSSIBLE
+    // Enable click to add
+
+    // Plant successfully selected -> need to add to island
+    setCurrentPlant(selectedPlant);
+  };
+
+  const handleIslandUpdate = (index) => {
+    //TODO CHECK IF THERE IS A PLANT EXISTING ALREADY (DENY THEM)
+
+    //Check if no plant selected (do nothing)
+    if (currentPlant == "") {
+    } else {
+      console.log(currentIslandDisplay, currentPlant);
+      console.log("HERE")
+      console.log(currentIslandDisplay[(0, index)])
+      console.log(currentPlant)
+      console.log([currentPlant])
+      console.log(currentIslandDisplay[(0, index)].concat([currentPlant]))
+
+
+      const firstHalf = currentIslandDisplay.slice(0, index).concat([currentPlant]);
+      console.log("firsthalf");
+      console.log(firstHalf);
+      const secondHalf = firstHalf.concat(
+        currentIslandDisplay.slice(index + 1, 23)
+      );
+      console.log("secondhalf)");
+      console.log(secondHalf);
+
+      setCurrentIslandDisplay(secondHalf);
+      // setCurrentIslandDisplay([BirdTree,"","","","","","","","","","","","","","","","","","","","","",""]);
+      console.log("hey");
+      console.log(currentIslandDisplay);
+
+      setCurrentPlant("");
+    }
+  };
 
   return (
     <>
@@ -36,17 +101,11 @@ export default function GardenPage() {
           <NavLink exact to="/" activeClassName="active">
             <img className="roadmap-button" src={RoadmapButton} />
           </NavLink>
-          <img
-            onClick={() => {
-              setCurrentIslandDisplay(
-                [BirdTree].concat(currentIslandState[(1, 23)])
-              );
-            }}
-            className="roadmap-button"
-            src={RoadmapButton}
-          />
+          <img className="roadmap-button" src={RoadmapButton} />
 
           <h1>{currentIslandDisplay[0]}</h1>
+          <h1>{currentPlant}</h1>
+
           <div className="store-bg">
             <div className="shop-heading">
               <h3 className="shop-title">Store</h3>
@@ -54,10 +113,21 @@ export default function GardenPage() {
             </div>
 
             <div className="shop-listings">
-                <img className="listing" src={BirdTreeListing} />
-                <img className="listing" src={FloweringTreeListing} />
-                <h4>sdf</h4>
-
+              <img
+                onClick={() => {
+                  handlePurchase(BirdTree);
+                }}
+                className="listing"
+                src={BirdTreeListing}
+              />
+              <img
+                onClick={() => {
+                  handlePurchase(FloweringTree);
+                }}
+                className="listing"
+                src={FloweringTreeListing}
+              />
+              <h4>sdf</h4>
             </div>
           </div>
         </div>
@@ -74,22 +144,49 @@ export default function GardenPage() {
                 <div id="1" className="grid-box placeholder"></div>
                 <div id="1" className="grid-box placeholder"></div>
 
-                <div id="1" className="grid-box valid">
+                <div
+                  id="0"
+                  onClick={() => {
+                    handleIslandUpdate(0);
+                  }}
+                  className="grid-box valid"
+                >
                   <img className="grid-icon" src={currentIslandDisplay[0]} />
                 </div>
-                <div id="1" className="grid-box valid">
+                <div
+                  id="1"
+                  onClick={() => {
+                    handleIslandUpdate(1);
+                  }}
+                  className="grid-box valid"
+                >
                   <img className="grid-icon" src={currentIslandDisplay[1]} />
                 </div>
-                <div id="1" className="grid-box valid">
-                  {" "}
+                <div
+                  id="2"
+                  onClick={() => {
+                    handleIslandUpdate(2);
+                  }}
+                  className="grid-box valid"
+                >
                   <img className="grid-icon" src={currentIslandDisplay[2]} />
                 </div>
-                <div id="1" className="grid-box valid">
-                  {" "}
+                <div
+                  id="3"
+                  onClick={() => {
+                    handleIslandUpdate(3);
+                  }}
+                  className="grid-box valid"
+                >
                   <img className="grid-icon" src={currentIslandDisplay[3]} />
                 </div>
-                <div id="1" className="grid-box valid">
-                  {" "}
+                <div
+                  id="4"
+                  onClick={() => {
+                    handleIslandUpdate(4);
+                  }}
+                  className="grid-box valid"
+                >
                   <img className="grid-icon" src={currentIslandDisplay[4]} />
                 </div>
 
