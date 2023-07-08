48339b98a6ec11f4b018112b3dd82749de2a817d
nroh555 <nroh555@aucklanduni.ac.nz>
2023-07-09 00:08:21 +1200

    Initialised the backend

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,17 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const port = 3000
 
-const app = express();
-const port = 3001;
-const MONGODB_CONNECTION_STRING =
-    "mongodb+srv://wouy448:0e4mXx1QTFCkMkQq@cluster0.hhelkph.mongodb.net"
-
-app.use(cors());
+app.use(express.urlencoded({extended: false}))
 app.use(express.json());
 
-import api from "./routes/index.js";
-app.use("/api", api);
+app.get('/', (req,res) => {
+    res.send("It's working")
+})
 
-mongoose.connect(MONGODB_CONNECTION_STRING + "/grass")
-    .then(() => {
-        console.log("Database connected");
-    })
-    .catch(error => {
-        console.error(error);
-    });
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+})
 
-app.listen(port, () => console.log(`App server listening on port ${port}!`));
 
