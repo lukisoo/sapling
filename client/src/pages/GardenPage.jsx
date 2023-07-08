acd4e01b4044a322b627fe10e4a4f22b37db5532
Lucy Zhu <lukisoo@wifi-172-23-173-228.uoa-wifi.auckland.ac.nz>
2023-07-09 01:21:28 +1200

    garden basic layout

--- client/src/pages/GardenPage.jsx
+++ client/src/pages/GardenPage.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import "./GardenPage.css";
-import RoadmapButton from "../assets/return-to-roadmap-button.png"
+import RoadmapButton from "../assets/return-to-roadmap-button.png";
+import IslandBg from "../assets/island.png";
+import { NavLink } from "react-router-dom";
 // import Xx from "../assets/dirt.svg"
 
 export default function GardenPage() {
-    return (
-        <>
-            <div className="wrapper">
-                {/* <RoadmapButton/> */}
-                <img src={RoadmapButton}/>
-                <div>
-                    <h1>fhsodifh</h1>
-                <div className="store-bg"></div></div>
-                <div>Two</div>
+  return (
+    <>
+      <div className="wrapper">
+        <div className="left">
+          <NavLink exact to="/" activeClassName="active">
+            <img className="roadmap-button" src={RoadmapButton} />
+          </NavLink>
+          <h1>fhsodifh</h1>
+          <div className="store-bg"></div>
+        </div>
+
+        <div className="right">
+          <div className="island-container">
+            {/* <img className="island" src={IslandBg} /> */}
+            <div className="island-overlay">
+              <h2>test</h2>
+              <h1>IDHFOSDIFHOSDIFH</h1>
             </div>
-        </>);
-};
+          </div>
+
+          <div className="info-bg"></div>
+        </div>
+      </div>
+    </>
+  );
+}
