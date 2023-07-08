b450ecb233633add38dc8d3b71dfd72b905aff56
nroh555 <nroh555@aucklanduni.ac.nz>
2023-07-09 10:05:10 +1200

    Added cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
+const cors = require('cors')
 const port = 3000
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req,res) => {
     res.send("It's working")
