c1920868363d4020d6dd00c3bb6140d54781d77a
dtor068 <dtor068@aucklanduni.ac.nz>
2023-07-08 21:43:25 +1200

    started work on home page assets

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function HomePage() {
-    return <h1>Home</h1>;
+
+    const stickyStyle = {
+        position: 'fixed',
+        left: 0,
+        bottom: 0,
+        width: '100%',
+      };
+
+    return (
+        <div>
+            <div>
+                <h1>Home</h1>
+                <img src="node_finished.svg" alt="Node 1" />
+            </div>
+
+        <div style={stickyStyle}>
+            <img src="home_footer.svg" alt="Home Footer" />
+        </div>
+    </div>
+    );
 };
