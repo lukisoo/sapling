eb5b7c91f113041e88a6730147648ada104eb7d9
Matthew Ouyang <43261675+mouyang2001@users.noreply.github.com>
2023-07-08 18:52:32 +1200

    connect to mongo db atlas

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,25 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3001;
-app.use(cors());
+const MONGODB_CONNECTION_STRING =
+    "mongodb+srv://wouy448:0e4mXx1QTFCkMkQq@cluster0.hhelkph.mongodb.net"
 
+app.use(cors());
 app.use(express.json());
 
-import api from "./routes";
+import api from "./routes/index.js";
 app.use("/api", api);
 
+mongoose.connect(MONGODB_CONNECTION_STRING + "/grass")
+    .then(() => {
+        console.log("Database connected");
+    })
+    .catch(error => {
+        console.error(error);
+    });
 
 app.listen(port, () => console.log(`App server listening on port ${port}!`));
 
