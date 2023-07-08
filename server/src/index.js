6f958712a83478cd531f08ed5207b55556d46912
nroh555 <nroh555@aucklanduni.ac.nz>
2023-07-09 00:17:29 +1200

    Added Slack API webhooks

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,14 @@ app.get('/', (req,res) => {
     res.send("It's working")
 })
 
+app.post('/', (req,res) => {
+    axios.post('https://hooks.slack.com/services/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', {text: 'tbd'})
+}).then(()=> {
+    res.send("Success")
+}).catch(() => {
+    res.send("Fail")
+})
+
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`)
 })
