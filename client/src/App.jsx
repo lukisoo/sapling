8f7d55f5fdd1f8d556c584a78d3e1d95ccee78b6
Matthew Ouyang <43261675+mouyang2001@users.noreply.github.com>
2023-07-08 20:23:29 +1200

    add navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,17 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import GardenPage from "./pages/GardenPage.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
-    <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/garden" element={<GardenPage />}/>
-    </Routes>
+      <div>
+          <Navbar />
+          <Routes>
+              <Route path="/" element={<HomePage />}/>
+              <Route path="/garden" element={<GardenPage />}/>
+          </Routes>
+      </div>
   )
 }
 
