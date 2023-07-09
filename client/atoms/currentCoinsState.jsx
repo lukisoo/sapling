7b3c7bb1c8b3edc96103bd4bdbd5462848e33fd0
Lucy Zhu <lukisoo@wifi-172-23-138-125.uoa-wifi.auckland.ac.nz>
2023-07-09 16:50:50 +1200

    fjsoidjf

--- client/atoms/currentCoinsState.jsx
+++ client/atoms/currentCoinsState.jsx
@@ -5,5 +5,5 @@ export const currentCoinsState = atom({
   key: "currentCoinsState", // unique ID (with respect to other atoms/selectors)
   // default: 0, // default value (aka initial value)
 
-  default: 1582 // default value (aka initial value)
+  default: 1600 // default value (aka initial value)
 });
